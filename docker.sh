03a13aecc8aa8dacf3969b33ed542f248bc7f631
root <root@ubuntu01.xhi3n1m4n1dudlrujuynqucu2d.px.internal.cloudapp.net>
2024-11-07 16:44:07 +0000

    Commit

---
+++ docker.sh
@@ -0,0 +1,9 @@
+count=`sudo docker ps -a | wc -l`
+if [ $count -gt 1 ]
+then
+        sudo docker rm -f $(sudo docker ps -a -q)
+fi
+count1=`sudo docker images | wc -l`
+if [ $count1 -gt 1 ]
+then
+
